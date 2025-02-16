3202f67982b2183351664e836e5588a081210805
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-02-16 20:35:19 +0300

    feat:project start

---
+++ providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    region     = "ru-central1"
+    key        = "ус-deploy.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+
+  }
+}
+
+provider "yandex" {
+  zone      = "ru-central1-d"
+  token     = var.token
+  folder_id = var.folder_id
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "token" {
+  type      = string
+  sensitive = true
+}
+
+variable "folder_id" {
+  type      = string
+  sensitive = true
+}
