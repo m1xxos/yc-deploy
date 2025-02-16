2ba26956f86eccbdef0a01ca87e68744ca0b46c9
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-02-16 22:04:12 +0300

    feat:k8s node group

--- k8s.tf
+++ k8s.tf
@@ -29,3 +29,37 @@ resource "yandex_kubernetes_cluster" "kube-infra" {
   node_service_account_id = yandex_iam_service_account.kube-infra.id
   release_channel         = "RAPID"
 }
+
+resource "yandex_kubernetes_node_group" "group-1" {
+  name = "group-1"
+  cluster_id = yandex_kubernetes_cluster.kube-infra.id
+  version = "1.29"
+
+  instance_template {
+    resources {
+      cores = 2
+      memory = 4
+    }
+
+    network_interface {
+      nat = true
+      subnet_ids = [yandex_vpc_subnet.default-ru-central1-d.id]
+      security_group_ids = [yandex_vpc_security_group.yc-security-group.id]
+    }
+
+    scheduling_policy {
+      preemptible = true
+    }
+
+    metadata = {
+      "ssh-keys": "m1xxos:${file("~/.ssh/id_rsa.pub")}"
+    }
+  }
+  scale_policy {
+    auto_scale {
+      initial = 1
+      min = 1
+      max = 2
+    }
+  }
+}
