cd9a6f5023cf2685d886d10dc91cad00d5b4e89a
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-02-16 21:27:19 +0300

    feat:subnets

--- base.tf
+++ base.tf
@@ -7,6 +7,24 @@ resource "yandex_vpc_network" "default" {
   name = "default"
 }
 
+resource "yandex_vpc_subnet" "default-ru-central1-a" {
+  name           = "default-ru-central1-a"
+  v4_cidr_blocks = ["10.127.0.0/24"]
+  network_id     = yandex_vpc_network.default.id
+}
+
+resource "yandex_vpc_subnet" "default-ru-central1-b" {
+  name           = "default-ru-central1-b"
+  v4_cidr_blocks = ["10.128.0.0/24"]
+  network_id     = yandex_vpc_network.default.id
+}
+
+resource "yandex_vpc_subnet" "default-ru-central1-d" {
+  name           = "default-ru-central1-d"
+  v4_cidr_blocks = ["10.129.0.0/24"]
+  network_id     = yandex_vpc_network.default.id
+}
+
 resource "yandex_vpc_security_group" "yc-security-group" {
   name        = "yc-security-group"
   description = "yc deploy sg"
