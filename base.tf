bdd822169579f81cedf9757217cb4800a38a1ad6
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-02-16 21:48:37 +0300

    fix:subnet zones

--- base.tf
+++ base.tf
@@ -11,18 +11,21 @@ resource "yandex_vpc_subnet" "default-ru-central1-a" {
   name           = "default-ru-central1-a"
   v4_cidr_blocks = ["10.127.0.0/24"]
   network_id     = yandex_vpc_network.default.id
+  zone           = "ru-central1-a"
 }
 
 resource "yandex_vpc_subnet" "default-ru-central1-b" {
   name           = "default-ru-central1-b"
   v4_cidr_blocks = ["10.128.0.0/24"]
   network_id     = yandex_vpc_network.default.id
+  zone           = "ru-central1-b"
 }
 
 resource "yandex_vpc_subnet" "default-ru-central1-d" {
   name           = "default-ru-central1-d"
   v4_cidr_blocks = ["10.129.0.0/24"]
   network_id     = yandex_vpc_network.default.id
+  zone           = "ru-central1-d"
 }
 
 resource "yandex_vpc_security_group" "yc-security-group" {
