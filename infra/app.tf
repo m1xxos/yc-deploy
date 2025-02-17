67a416f83ce5c8a2845c1efdd270f3a1d2690c9a
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-02-17 21:22:34 +0300

    feat:k8s deployment

--- infra/app.tf
+++ infra/app.tf
@@ -1,2 +1,7 @@
+resource "kubernetes_manifest" "httpbin" {
+  manifest = yamldecode(file("./manifests/deployment.yaml"))
+}
 
-
+resource "kubernetes_manifest" "httpbin-service" {
+  manifest = yamldecode(file("./manifests/service.yaml"))
+}
--- infra/providers.tf
+++ infra/providers.tf
@@ -29,7 +29,7 @@ provider "yandex" {
 
 provider "kubernetes" {
 
-  host                   = data.yandex_kubernetes_cluster.kubernetes.master.0.external_v4_endpoint
-  cluster_ca_certificate = data.yandex_kubernetes_cluster.kubernetes.master.0.cluster_ca_certificate
+  host                   = data.yandex_kubernetes_cluster.kube-infra.master.0.external_v4_endpoint
+  cluster_ca_certificate = data.yandex_kubernetes_cluster.kube-infra.master.0.cluster_ca_certificate
   token                  = data.yandex_client_config.client.iam_token
 }
