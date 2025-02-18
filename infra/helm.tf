16eef416dd78ae0884fb562af4df2f6b5d60176d
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-02-18 21:31:29 +0300

    feat:helm secrets

--- infra/helm.tf
+++ infra/helm.tf
@@ -5,4 +5,15 @@ resource "helm_release" "argocd" {
   chart            = "argo-cd"
   create_namespace = true
   version          = "5.46.8-6"
+  values           = [file("./manifests/argocd.yaml.dec")]
+}
+
+resource "kubernetes_secret" "helm-secrets-private-keys" {
+  metadata {
+    name = "helm-secrets-private-keys"
+    namespace = "argocd"
+  }
+  data = {
+    "key.txt" = file("./key.txt")
+  }
 }
