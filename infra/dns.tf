d6cf941729ad86e3ba0eef6cf14a5b7d43a9638b
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-02-24 20:05:53 +0300

    fix:zone base domain

--- infra/dns.tf
+++ infra/dns.tf
@@ -1,19 +1,23 @@
 resource "yandex_cm_certificate" "kube-infra" {
   name    = "kube-infra"
-  domains = ["*.infra.m1xxos.me"]
+  domains = ["*.${var.environment}-infra.m1xxos.me"]
 
   managed {
     challenge_type = "DNS_CNAME"
   }
+
+  labels = {
+    environment = var.environment
+  }
+  depends_on = [ yandex_resourcemanager_folder.yc-deploy ]
 }
 
 resource "cloudflare_record" "infra" {
   zone_id = var.cloudflare_zone_id
-  name    = "*.infra.m1xxos.me"
+  name    = yandex_cm_certificate.kube-infra.domains[0]
   content = yandex_vpc_address.infra-alb.external_ipv4_address[0].address
   type    = "A"
   ttl     = 300
-  depends_on = [ yandex_resourcemanager_folder.yc-deploy ]
 }
 
 resource "cloudflare_record" "acme" {
